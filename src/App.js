cb4842d476fb5f0cb16e7dd23979683498344ca7
JeevaFsd-0 <rpjeeva17@gmail.com>
2023-09-20 13:55:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import questions from "./data";
+import "./App.css";
+
+const App = () => {
+
+  //to display the current question
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  //to either show questions or scoreboard
+  const [showScore, setShowScore] = useState(false);
+  //to track score
+  const [score, setScore] = useState(0);
+
+  const handleAnswerOptionClick = (isCorrect)=>{
+    if(isCorrect){
+      setScore((currScore)=>currScore+1);
+    }
+    const nextQuestion = currentQuestion + 1;
+    if(nextQuestion<questions.length){
+      setCurrentQuestion(nextQuestion);
+    }
+    else{
+      setShowScore(true);
+    }
+  };
+
+  const restart = () => {
+    setCurrentQuestion(0);
+    setShowScore(false);
+    setScore(0);
+  }
+
+  return <div className="app">
+    {showScore ? (
+      <>
+        <div className="score-container">
+          <div className="score-section">
+            you scored {score} out of {questions.length}
+          </div>
+          <button className="btn" onClick={restart}>Restart</button>
+        </div>
+      </>
+      
+    ) : (
+      <>
+        <div className="question-section">
+          <div className="question-count">
+            <span>Question {currentQuestion + 1}</span> / {questions.length}
+          </div>
+          <div className="question-text">
+            {questions[currentQuestion].questionText}
+          </div>
+          <div>
+            {questions[currentQuestion].answerOption.map(
+              (answerOption, index) => {
+                return (
+                  <button 
+                    key={index} 
+                      onClick={()=>handleAnswerOptionClick(answerOption.isCorrect)
+                      }
+                    >
+                      {answerOption.answerText}
+                  </button>
+                );
+              }
+            )}
+          </div>
+        </div>
+      </>
+    )}
+  </div>;
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        questionText: "Who is the Cover Drive King of Cricket?",
+        answerOption: [
+            { answerText: "Hardik Pandya", isCorrect: false },
+            { answerText: "MS Dhoni", isCorrect: false },
+            { answerText: "Virat Kholi", isCorrect: true },
+            { answerText: "Rohit Sharma", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Who is Called Ultimate Star in Tamil Film Industry?",
+        answerOption: [
+            { answerText: "Vikram", isCorrect: false },
+            { answerText: "Ajith Kumar", isCorrect: true },
+            { answerText: "Vijay", isCorrect: false },
+            { answerText: "Siva Karthikeyan", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "Which Color is the Brand of KTM?",
+        answerOption: [
+            { answerText: "Orange", isCorrect: true },
+            { answerText: "Blue", isCorrect: false },
+            { answerText: "Red", isCorrect: false },
+            { answerText: "Purple", isCorrect: false },
+        ],
+    },
+    {
+        questionText: "MS Dhoni's Jercy Number?",
+        answerOption: [
+            { answerText: "18", isCorrect: false },
+            { answerText: "45", isCorrect: false },
+            { answerText: "3", isCorrect: false },
+            { answerText: "7", isCorrect: true },
+        ],
+    },
+];
